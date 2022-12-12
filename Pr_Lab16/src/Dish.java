e46eae06904ce93c90dd9930c146bc5fbcc4d53a
sherri <serfertor@mail.ru>
2022-12-13 02:42:08 +0300

    pr16-end+

--- Pr_Lab16/src/Dish.java
+++ Pr_Lab16/src/Dish.java
@@ -3,6 +3,22 @@ public final class Dish implements Item{
     private String name;
     private String description;
 
+    public Dish(String name, String description) throws IllegalArgumentException {
+        if (name.isEmpty() && description.isEmpty()){
+            throw new IllegalArgumentException();
+        }
+        this.name = name;
+        this.description = description;
+        this.price = 0;
+    }
+    public Dish(int price, String name, String description) {
+        if (name.isEmpty() && description.isEmpty() && price < 0){
+            throw new IllegalArgumentException();
+        }
+        this.price = price;
+        this.name = name;
+        this.description = description;
+    }
     public int getPrice() {
         return price;
     }
--- Pr_Lab16/src/Drink.java
+++ Pr_Lab16/src/Drink.java
@@ -3,13 +3,19 @@ public final class Drink implements Item{ //immutable class
     private String name;
     private String description;
 
-    public Drink(String name, String description) {
+    public Drink(String name, String description) throws IllegalArgumentException {
+        if (name.isEmpty() && description.isEmpty()){
+            throw new IllegalArgumentException();
+        }
         this.name = name;
         this.description = description;
         this.price = 0;
     }
 
     public Drink(int price, String name, String description) {
+        if (name.isEmpty() && description.isEmpty() && price < 0){
+            throw new IllegalArgumentException();
+        }
         this.price = price;
         this.name = name;
         this.description = description;
